2c593695bc91763bfd0ef248c5b1513bdd48724d
josealexandre87 <jose.josealexandre87@gmail.com>
2024-09-06 18:41:51 -0300

    inseridos comentários e explicações sobre os códigos

--- Estudando_a_API/003_desafio_chatbot_com_python.py
+++ Estudando_a_API/003_desafio_chatbot_com_python.py
@@ -1,11 +1,15 @@
 import openai
 from dotenv import find_dotenv, load_dotenv
 
+# Carrega variáveis de ambiente a partir do arquivo .env, como a chave da API
 _ = load_dotenv(find_dotenv())
 
+# Cria uma instância do cliente OpenAI para realizar chamadas à API
 client = openai.Client()
 
+# Função para gerar respostas do modelo com base nas mensagens fornecidas
 def geracao_de_texto(mensagens, model='gpt-3.5-turbo.0125', max_tokens=1000, temperature=0):
+    # Envia as mensagens para a API da OpenAI e recebe a resposta em formato de stream (em pedaços)
     resposta = client.chat.completions.create(
         messages=mensagens,
         model=model,
@@ -14,23 +18,24 @@ def geracao_de_texto(mensagens, model='gpt-3.5-turbo.0125', max_tokens=1000, tem
         stream=True, # Ativa o modo de streaming para obter a resposta em partes (chunks).
     )  
     
-    print('Assintant', end='')
-    texto_completo = ''
+    print('Assintant', end='') # Imprime a indicação de que o assistente está respondendo
+    texto_completo = '' # Variável para armazenar a resposta completa
     for resposta_stream in resposta:
-        texto = resposta_stream.choices[0].delta.content
+        texto = resposta_stream.choices[0].delta.content # Extrai o conteúdo do pedaço atual
         if texto:
             print(texto, end='')
             texto_completo += texto
     print()
-    
+    # Adiciona a resposta completa do assistente na lista de mensagens
     mensagens.append({'role': 'assistent', 'content': texto_completo})
     return mensagens
 
+# Execução principal do chatbot
 if __name__ == '__main__':
     
     print('Bem-vindo ao chatBot com Python!')
     mensagens = []
     while True:
         input_usuario = input('User: ')
-        mensagens.append({'role': 'user', 'content': input_usuario})
+        mensagens.append({'role': 'user', 'content': input_usuario}) # no caso seria o PROMPT!
         mensagens = geracao_de_texto(mensagens)
